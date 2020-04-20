a4bab9ec0ec1bad0119d80354d917cbc45c9d83b
h578043 <42607311+h578043@users.noreply.github.com>
2020-04-20 10:31:30 +0200

    Add files via upload

---
+++ ChordDHTPeer-Project3/ChordDHTPeer-Project3/src/module-info.java
@@ -0,0 +1,11 @@
+module a {
+	exports no.hvl.dat110.util;
+	exports no.hvl.dat110.main;
+	exports no.hvl.dat110.chordoperations;
+	exports no.hvl.dat110.middleware;
+	exports no.hvl.dat110.node.peers;
+	exports no.hvl.dat110.rpc.interfaces;
+
+	requires java.desktop;
+	requires java.rmi;
+}
